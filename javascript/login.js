cd7fd36eac2a46fd20c94f57391f216e44fa0ac6
Vinicius brodt <vini_brodt_@hotmail.com>
2023-02-11 13:13:05 -0300

    adicionado booststrap

--- javascript/login.js
+++ javascript/login.js
@@ -4,14 +4,13 @@ var usuarios = JSON.parse(localStorage.getItem('arrayUsuario'));
 let jsonTarefa =  localStorage.getItem('arrayUsuario');
 
 let tarefa = JSON.parse(jsonTarefa);
-const registerForm = document.getElementById("formu");
+const registerForm = document.getElementById("forms");
 registerForm.addEventListener('submit',(evento)=>{
     evento.preventDefault()
     let emailf = document.getElementById("email")
     let email = document.getElementById("email").value
     let senhaf = document.getElementById("password")
     let senha = document.getElementById("password").value
-
     if (jsonTarefa = localStorage.getItem('arrayUsuario') == null) {
         emailf.value = ''
         senhaf.value = ''
